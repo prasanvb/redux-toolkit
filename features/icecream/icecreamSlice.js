a795ec6621d3b9d590def4d463db7fff5ef13027
prasanv <prasan.venkatb@gmail.com>
2022-08-07 00:37:38 -0700

    extra reducer builder approach

--- features/icecream/icecreamSlice.js
+++ features/icecream/icecreamSlice.js
@@ -1,5 +1,5 @@
 import rtk from "@reduxjs/toolkit";
-
+import { cakeActions } from "../cake/cakeSlice.js";
 const initialState = {
   numOfIceCreams: 20,
 }
@@ -20,11 +20,20 @@ const iceCreamSlice = rtk.createSlice({
   // IMPORTANT NOTE: One of the key concepts of Redux is that each slice reducer "owns" its slice of state, 
   // and that many slice reducers can independently respond to the same action type. 
   // extraReducers allows createSlice to respond to other action types besides the types it has generated.
-  extraReducers: {
-  ['cakeStateSlice/ordered']: (currentState)  => {
-    currentState.numOfIceCreams--;
+  
+  // extraReducers: {
+  //   ['cakeStateSlice/ordered']: (currentState)  => {
+  //     currentState.numOfIceCreams--;
+  //   }
+  // }
+
+  // Recommended approach to add extra reducer is through a builder function
+  extraReducers: (builder) => {
+     builder.addCase(cakeActions.ordered, (currentState) => {
+      currentState.numOfIceCreams--;
+    })
   }
-  }
+  
 })
 
 
