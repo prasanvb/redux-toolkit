ec2690df33535dc9905c38aab767ae4a7f623d94
prasanv <bala.prasanv@gmail.com>
2023-01-03 22:24:57 -0800

    updated store

--- app/store.js
+++ app/store.js
@@ -3,26 +3,24 @@ import { cakeReducer } from "../features/cake/cakeSlice.js";
 import { iceCreamReducer } from "../features/iceCream/iceCreamSlice.js";
 import { userReducer } from "../features/user/userSlice.js";
 // Logger Middleware
-import logger from 'redux-logger'
+import logger from "redux-logger";
 
 // Configure store is used to defined a Store in Redux-Toolkit
-console.log(rtk.configureStore);
+console.log(rtk);
 
 const store = rtk.configureStore({
-  // IMPORTANT NOTE: This only works for one level of reducers. 
-  // If you want to nest reducers, you'll need to call 
+  // IMPORTANT NOTE: This only works for one level of reducers.
+  // If you want to nest reducers, you'll need to call
   // `combineReducers` yourself to handle the nesting.
-  reducer:{
+  reducer: {
     cakes: cakeReducer,
     iceCreams: iceCreamReducer,
-    users: userReducer
+    users: userReducer,
   },
   // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-})
+});
 
 // Note: Check to see what all methods returned by the store
 // console.log(store);
 
 export { store };
-
-
--- features/user/userSlice.js
+++ features/user/userSlice.js
@@ -3,51 +3,51 @@
  */
 
 import rtk from "@reduxjs/toolkit";
-import axios from 'axios';
-
+import axios from "axios";
 
 const initialState = {
   loading: false,
   users: [],
-  error: ''
-}
+  error: "",
+};
 
 // createAsyncThunk(actionType, functionCallback)
 // functionCallback performs async action and return a promise
 // you can listen to the promise lifecycle action using extraReducers
 
-const fetchUsers = rtk.createAsyncThunk('user/fetchUsers', ()=>{
- return axios
+const fetchUsers = rtk.createAsyncThunk("user/fetchUsers", () => {
+  return axios
     .get("https://jsonplaceholder.typicode.com/users")
-    .then(response => {
-      console.log('Success:', response.data.map(user => user.id)); 
-    })
+    .then((response) => {
+      //console.log(response);
+      console.log(
+        "Success:",
+        response.data.map((user) => user.name)
+      );
+    });
+});
 
-})
-
-const userSlice =rtk.createSlice({
-  name: 'user',
+const userSlice = rtk.createSlice({
+  name: "user",
   initialState,
   // listening to the promise lifecycle action using extraReducers
-  extraReducers: builder => {
-    builder.addCase(fetchUsers.pending, state => {
-      state.loading = true
-    })
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.loading = true;
+    });
     builder.addCase(fetchUsers.fulfilled, (state, action) => {
-      state.loading = false
-      state.users = action.payload
-      state.error = ''
-    })
+      state.loading = false;
+      state.users = action.payload;
+      state.error = "";
+    });
     builder.addCase(fetchUsers.rejected, (state, action) => {
-      state.loading = false
-      state.users = []
-      state.error = action.error.message
-    })
-  }
-})
+      state.loading = false;
+      state.users = [];
+      state.error = action.error.message;
+    });
+  },
+});
 
 const userReducer = userSlice.reducer;
 
-export {userReducer, fetchUsers};
-
-
+export { userReducer, fetchUsers };
